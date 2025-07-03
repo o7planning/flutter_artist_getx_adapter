6b61a2b319587f615887673234b35852e0275400
o7planning <10128477+o7planning@users.noreply.github.com>
2025-07-04 01:10:23 +0600

    .

--- lib/flutter_artist_getx_adapter.dart
+++ lib/flutter_artist_getx_adapter.dart
@@ -5,4 +5,4 @@ import 'package:get/get.dart';
 part 'src/_app_base_controller.dart';
 part 'src/_flutter_artist_getx_adapter.dart';
 part 'src/_progress_mixin.dart';
-part 'src/_snackbar_mixin.dart';
+part 'src/_snack_bar_mixin.dart';
