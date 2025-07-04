8d6cb7ee7ce1144c7f033c9031d2996447282a36
o7planning <10128477+o7planning@users.noreply.github.com>
2025-07-04 14:58:52 +0600

    .

--- lib/src/_flutter_artist_getx_locale_adapter.dart
+++ lib/src/_flutter_artist_getx_locale_adapter.dart
@@ -1,8 +1,18 @@
 part of '../flutter_artist_getx_adapter.dart';
 
 class FlutterArtistGetxLocaleAdapter implements ILocaleAdapter {
+  final List<Locale> _supportedLocales;
+
+  FlutterArtistGetxLocaleAdapter({required List<Locale> supportedLocales})
+      : _supportedLocales = supportedLocales;
+
   @override
   Future<void> updateLocale(Locale locale) async {
-    Get.updateLocale(locale);
+    await Get.updateLocale(locale);
+  }
+
+  @override
+  List<Locale> supportedLocales() {
+    return _supportedLocales;
   }
 }
